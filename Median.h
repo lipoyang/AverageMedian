3ac86941c5603bb96562b589207facacb07d01a9
Bizan Nishimura <lipoyang1453@gmail.com>
2019-07-30 18:03:03 +0900

    add files

---
+++ Median.h
@@ -0,0 +1,117 @@
+#ifndef _MEDIAN_H_
+#define _MEDIAN_H_
+
+// 線形リスト構造体
+template <typename TYPE>
+struct MedianList {
+    TYPE value;
+    MedianList *next;
+};
+
+// 移動中央値演算器
+template <typename TYPE>
+class Median
+{
+public:
+    TYPE value;             // 中央値
+
+private:
+    int  size;              // 線形リストのバッファのサイズ
+    MedianList<TYPE> *buff; // 線形リストのバッファ
+    MedianList<TYPE> root;  // 線形リストのルート
+    int   index;            // インデックス
+    bool  isFirst;          // 最初のサンプルか？
+
+public:
+    // コンストラクタ
+    Median(MedianList<TYPE> *buff, int size)
+    {
+        this->buff = buff;
+        this->size = size;
+        this->isFirst = true;
+    }
+
+    // 初期化する
+    void init()
+    {
+        isFirst = true;
+    }
+
+    // 中央値を計算する
+    // input_val: 入力データ
+    // return: 中央値
+    TYPE calc(TYPE input_val)
+    {
+        if (isFirst)
+        {
+            // 最初のデータときに線形リストを初期化する。
+            MedianList<TYPE> *node = &root;
+            for (int i = 0; i < size; i++) {
+                MedianList<TYPE> *new_node = &buff[i];
+                new_node->value = input_val;
+                node->next = new_node;
+                node = node->next;
+            }
+            value = input_val;
+            index = 0;
+            isFirst = false;
+        }
+        else
+        {
+            // (1) いちばん古いデータを捨てる
+            MedianList<TYPE> *node = &root;
+            for (int i = 0; i < size; i++) {
+                if (node->next == &buff[index]) {
+                    // つなぎかえ(ノード削除)
+                    node->next = node->next->next;
+                    break;
+                }
+                else {
+                    node = node->next;
+                }
+            }
+            // (2) 新しいデータを挿入
+            MedianList<TYPE> *new_node = &buff[index]; // (1)で削除したノードの再利用
+            new_node->value = input_val;
+            node = &root;
+            bool inserted = false;
+            for (int i = 0; i < size - 1; i++) {
+                if (new_node->value > node->next->value) {
+                    // つなぎかえ(ノード挿入)
+                    new_node->next = node->next;
+                    node->next = new_node;
+                    inserted = true;
+                    break;
+                }
+                else {
+                    node = node->next;
+                }
+            }
+            if (!inserted) {
+                node->next = new_node;
+            }
+            // (3) 中央値を取得
+            node = &root;
+            for (int i = 0; i < size / 2; i++) {
+                node = node->next;
+            }
+            if ((size & 1) == 0) {
+                value = (node->value + node->next->value) / 2; // 偶数個なら中央の2個の平均を取る
+            }else{
+                value = node->next->value; // 奇数個なら中央の値を取る
+            }
+
+            // index更新
+            index++;
+            if (index >= size) index = 0;
+        }
+        return value;
+    }
+};
+
+// Medianのインスタンスを生成するマクロ
+#define Median_create(TYPE, size, name) \
+    MedianList<TYPE> _ ## name ## _buff[size]; \
+    Median<TYPE> name(_ ## name ## _buff, size)
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,54 @@
+// 移動平均値と移動中央値のテスト
+
+#include <stdio.h>
+#include <math.h>
+#include "Average.h"
+#include "Median.h"
+#include "test_data.h" // テスト用入力データと期待値データ (Excelで作成)
+
+// float型・32サンプルの移動平均値演算器の生成 (マクロ)
+Average_create(float, 32, average);
+// float型・32サンプルの移動中央値演算器の生成 (マクロ)
+Median_create(float, 32, median);
+
+int main(void)
+{
+    int error = 0;
+
+    // 移動平均値のテスト
+    average.init(); // 初期化
+    for (int i = 0; i < DATA_SIZE; i++) {
+        float output_val = average.calc(input_data[i]); // 移動平均値の計算
+
+        if (fabs(output_val - average_data[i]) > 0.001) { // 実数型なので完全一致でなく誤差で判定
+            error++;
+            printf("ERROR! ");
+            printf("%f %f %f\n", input_data[i], average_data[i], output_val);
+        }
+    }
+    if (error == 0) {
+        printf("Average OK\n");
+    }else{
+        printf("Average NG\n");
+    }
+
+    // 移動中央値のテスト
+    error = 0;
+    median.init(); // 初期化
+    for (int i = 0; i < DATA_SIZE; i++) {
+        float output_val = median.calc(input_data[i]); // 移動中央値の計算
+
+        if (fabs(output_val - median_data[i]) > 0.001) { // 実数型なので完全一致でなく誤差で判定
+            error++;
+            printf("ERROR! ");
+            printf("%d %f %f %f\n", i, input_data[i], median_data[i], output_val);
+        }
+    }
+    if (error == 0) {
+        printf("Median OK\n");
+    }else{
+        printf("Median NG\n");
+    }
+
+    return 0;
+}
